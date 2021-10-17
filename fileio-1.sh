78df768dce5a9adc0b3d46ab0559c3fce1599537
Abhilash Harish Srivathsa <abhilashsrivatsa4656@gmail.com>
2021-10-16 20:22:20 -0700

    Update fileio-1.sh

--- fileio-1.sh
+++ fileio-1.sh
@@ -5,9 +5,9 @@
 i=1
 while [ $1 -lt 6 ]
 do
-      sysbench --test=fileio --file-total-size=4G --file-test-mode=rndrw --max-time=30 --max-requests=0 --file-extra-flags= direct prepare
-      sysbench --test=fileio --file-total-size=4G --file-test-mode=rndrw --max-time=30 --max-requests=0 --file-extra-flags= direct run
-      sleep(1000)
-      sysbench --test=fileio --file-total-size=4G --file-test-mode=rndrw --max-time=30 --max-requests=0 --file-extra-flags= direct cleanup
+      sysbench --test=fileio --file-total-size=4G --file-test-mode=rndrw --max-time=30 --max-requests=0 --file-extra-flags=direct prepare
+      sysbench --test=fileio --file-total-size=4G --file-test-mode=rndrw --max-time=30 --max-requests=0 --file-extra-flags=direct run
+      sleep 1000
+      sysbench --test=fileio --file-total-size=4G --file-test-mode=rndrw --max-time=30 --max-requests=0 --file-extra-flags=direct cleanup
       i=`expr $i + 1`
 done
