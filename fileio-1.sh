89d941e711451c5442f088b1152bad234e59ffe4
Abhilash Harish Srivathsa <abhilashsrivatsa4656@gmail.com>
2021-10-16 20:10:40 -0700

    Update fileio-1.sh

--- fileio-1.sh
+++ fileio-1.sh
@@ -2,6 +2,12 @@
 
 #Clear cache on host system after every run
 
-sysbench --num-threads=16 --test=fileio --file-total-size=3G --file-test-mode=rndrw prepare
-sysbench --num-threads=16 --test=fileio --file-total-size=3G --file-test-mode=rndrw run
-sysbench --num-threads=16 --test=fileio --file-total-size=3G --file-test-mode=rndrw cleanup
+i=1
+while [ $1 -lt 6 ]
+do
+      sysbench --test=fileio --file-total-size=4G --file-test-mode=rndrw --max-time=30 --max-requests=0 --file-extra-flags= direct prepare
+      sysbench --test=fileio --file-total-size=4G --file-test-mode=rndrw --max-time=30 --max-requests=0 --file-extra-flags= direct run
+      sleep(1000)
+      sysbench --test=fileio --file-total-size=4G --file-test-mode=rndrw --max-time=30 --max-requests=0 --file-extra-flags= direct cleanup
+      i=`expr $i + 1`
+done
