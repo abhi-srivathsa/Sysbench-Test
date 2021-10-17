1e0925a0cb39dd0e0e934e04904f73583bd23661
Abhilash Harish Srivathsa <abhilashsrivatsa4656@gmail.com>
2021-10-17 16:17:46 -0700

    Create fileio-3.sh

---
+++ fileio-3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Clear cache on host system after every run
+
+i=1
+while [ $i -lt 6 ]
+do
+      sysbench --test=fileio --file-total-size=1G --file-test-mode=rndrw --max-time=30 --max-requests=0 --file-extra-flags=direct prepare
+      sysbench --test=fileio --file-total-size=1G --file-test-mode=rndrw --max-time=30 --max-requests=0 --file-extra-flags=direct run
+      sleep 60
+      sysbench --test=fileio --file-total-size=1G --file-test-mode=rndrw --max-time=30 --max-requests=0 --file-extra-flags=direct cleanup
+      i=`expr $i + 1`
+done
