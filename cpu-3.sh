706e531641cdc2b04e1bc91b3a87821884ad9bc7
Abhilash Harish Srivathsa <abhilashsrivatsa4656@gmail.com>
2021-10-17 16:18:58 -0700

    Update cpu-3.sh

--- cpu-3.sh
+++ cpu-3.sh
@@ -4,7 +4,7 @@ i=1
 while [ $i -lt 6 ]
 do
 	echo "Test number: $i" 
-	sysbench --test=cpu --cpu-max-prime=30000 run
+	sysbench --test=cpu --cpu-max-prime=10000 run
 	i=`expr $i + 1`
 	sleep 60
 done
